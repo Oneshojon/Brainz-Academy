a8734bbe6e2edf3100e18d1d42ac5aabb7107b88
TITUS DENIS <deetitos@gmail.com>
2026-03-27 23:46:38 +0100

    Fix ALLOWED_HOSTS and DEBUG to read from environment

--- examproject/settings.py
+++ examproject/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r&lhf(d=lmez(f#ene%%2o99#ze2u36xp%am(9ftc_d2+n5baz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
