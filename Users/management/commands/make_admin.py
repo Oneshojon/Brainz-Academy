752d137a241123d6965c443d4cd421bfc7d69569
TITUS DENIS <deetitos@gmail.com>
2026-03-29 16:53:43 +0100

    Simplify make_admin command - no args

--- Users/management/commands/make_admin.py
+++ Users/management/commands/make_admin.py
@@ -4,24 +4,15 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     help = 'Make a user superuser/admin'
 
-    def add_arguments(self, parser):
-        parser.add_argument('email', type=str)
-        parser.add_argument('password', type=str)
-
     def handle(self, *args, **options):
         User = get_user_model()
         try:
-            u = User.objects.get(email=options['email'])
+            u = User.objects.get(email='deetitos@gmail.com')
             u.is_staff = True
             u.is_superuser = True
             u.is_admin = True
-            u.set_password(options['password'])
+            u.set_password('Mautech2017')
             u.save()
-            self.stdout.write(f"Successfully made {options['email']} admin")
+            self.stdout.write('Successfully made admin')
         except User.DoesNotExist:
-            self.stdout.write(f"User {options['email']} not found")
-```
-
-Then set pre-deploy command to:
-```
-python manage.py migrate --noinput && python manage.py make_admin deetitos@gmail.com your-strong-password
+            self.stdout.write('User not found')
