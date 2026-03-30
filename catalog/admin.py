57ab599ada0b64c60860f7f62c0a6289b3945fa4
TITUS DENIS <deetitos@gmail.com>
2026-03-30 11:40:40 +0100

    Register all models in catalog admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from catalog.models import Worksheet, LessonNote
+from catalog.models import Worksheet, LessonNote, FeatureFlag, SubscriptionPlan, UserSubscription
 from .models import Subject, Topic, ExamBoard, ExamSeries, Question, Choice, TheoryAnswer
 from catalog.models import PastPaper
+
 
 @admin.register(PastPaper)
 class PastPaperAdmin(admin.ModelAdmin):
@@ -9,12 +10,14 @@ class PastPaperAdmin(admin.ModelAdmin):
     list_filter   = ['paper_type', 'exam_series__exam_board', 'exam_series__year']
     search_fields = ['exam_series__subject__name']
 
+
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
     list_display = ('name', 'subject')
     list_filter = ('subject',)
     search_fields = ('name',)
     ordering = ('subject', 'name')
+
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
@@ -34,3 +37,53 @@ class LessonNoteAdmin(admin.ModelAdmin):
     list_display  = ['topic', 'title', 'is_ai_generated', 'video_url', 'updated_at']
     list_filter   = ['is_ai_generated', 'topic__subject']
     search_fields = ['title', 'topic__name']
+
+
+@admin.register(ExamBoard)
+class ExamBoardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'abbreviation']
+
+
+@admin.register(ExamSeries)
+class ExamSeriesAdmin(admin.ModelAdmin):
+    list_display = ['exam_board', 'subject', 'year', 'sitting']
+    list_filter  = ['exam_board', 'subject', 'year']
+    search_fields = ['subject__name']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display  = ['question_number', 'subject', 'question_type', 'difficulty', 'exam_series']
+    list_filter   = ['subject', 'question_type', 'difficulty', 'exam_series__exam_board']
+    search_fields = ['content']
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['question', 'label', 'is_correct']
+    list_filter  = ['is_correct']
+
+
+@admin.register(TheoryAnswer)
+class TheoryAnswerAdmin(admin.ModelAdmin):
+    list_display = ['question']
+    search_fields = ['content']
+
+
+@admin.register(FeatureFlag)
+class FeatureFlagAdmin(admin.ModelAdmin):
+    list_display = ['label', 'key', 'is_enabled', 'visible_to', 'updated_at']
+    list_filter  = ['is_enabled', 'visible_to']
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ['name', 'plan_type', 'duration', 'price', 'is_active']
+    list_filter  = ['plan_type', 'duration', 'is_active']
+
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'plan', 'status', 'started_at', 'expires_at']
+    list_filter  = ['status', 'plan']
+    search_fields = ['user__email']
