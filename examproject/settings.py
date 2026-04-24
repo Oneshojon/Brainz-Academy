554d6ecdb493a958cad6d9cefd5b48fedba175f9
TITUS DENIS <deetitos@gmail.com>
2026-04-24 23:47:29 +0100

    remove hardcoded SECRET_KEY fallback

--- examproject/settings.py
+++ examproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-r&lhf(d=lmez(f#ene%%2o99#ze2u36xp%am(9ftc_d2+n5baz')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
